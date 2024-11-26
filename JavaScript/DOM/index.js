dfc6956993beb5dcd13ccfa1381b858bd18ebc59
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2024-11-26 22:27:37 +0530

    DOM

--- JavaScript/DOM/index.js
+++ JavaScript/DOM/index.js
@@ -1,1 +1,5 @@
-alert('Hello');
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+document.getElementsByClassName("item")[1].style.color = "brown";
+document.getElementById("title").innerHTML = "somnath";
+document.querySelector(".item").innerHTML = "s1";
+document.querySelector(".item").style.color = "red";
