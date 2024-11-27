c7737670f48d743476b4c571f596be2fc44f4e1d
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2024-11-27 07:08:58 +0530

    DOM completed

--- JavaScript/DOM/index.js
+++ JavaScript/DOM/index.js
@@ -1,5 +1,13 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
 document.getElementsByClassName("item")[1].style.color = "brown";
 document.getElementById("title").innerHTML = "somnath";
-document.querySelector(".item").innerHTML = "s1";
+//document.querySelector(".item").innerHTML = "s1";
 document.querySelector(".item").style.color = "red";
+
+document.querySelector("h1").style.color = "pink";
+//document.querySelector("button").classList.add("invisible");
+//document.querySelector("btn").style.backgroundColor = "yellow";
+
+//document.querySelector("h1").classList.add("invisible");
+
+document.querySelector("a").setAttribute("href", "https://www.x.com");
