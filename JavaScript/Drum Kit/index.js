02f70702ecc167a95af89dcbb8d1f2cd70e6d02c
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2024-11-30 20:35:36 +0530

    drum kit - background image and audio

--- JavaScript/Drum Kit/index.js
+++ JavaScript/Drum Kit/index.js
@@ -40,10 +40,49 @@
 
 const vecQuerySelClass = [".w", ".a", ".s", ".d", ".j", ".k", ".l"]; // document.queryslectorAll(".drum").length;
 
+// var s = "";
+// for (let s in vecQuerySelClass)
+// {
+//     document.querySelector(vecQuerySelClass[s]).addEventListener("click", function (){
+//         alert("Clicked " + vecQuerySelClass[s].toString()
+//         );
+//     });
+// }
+
 var s = "";
+var audio;
+var color;
 for (let s in vecQuerySelClass)
 {
     document.querySelector(vecQuerySelClass[s]).addEventListener("click", function (){
-        alert("Clicked " + vecQuerySelClass[s].toString());
+        //alert("Clicked " + vecQuerySelClass[s].toString());
+
+        //color = this.style.color;
+        //this.style.color = "white";
+        audio = new Audio();
+        
+        if (".w" == vecQuerySelClass[s].toString()) {
+            audio = new Audio('./sounds/tom-1.mp3');
+        } 
+        else if (".a" == vecQuerySelClass[s].toString()) {
+            audio = new Audio('./sounds/tom-2.mp3');
+        }
+        else if (".s" == vecQuerySelClass[s].toString()) {
+            audio = new Audio('./sounds/tom-3.mp3');
+        }
+        else if (".d" == vecQuerySelClass[s].toString()) {
+            audio = new Audio('./sounds/tom-4.mp3');
+        }
+        else if (".j" == vecQuerySelClass[s].toString()) {
+            audio = new Audio('./sounds/snare.mp3');
+        }
+        else if (".k" == vecQuerySelClass[s].toString()) {
+            audio = new Audio('./sounds/crash.mp3');
+        }
+        else if (".l" == vecQuerySelClass[s].toString()) {
+            audio = new Audio('./sounds/kick-bass.mp3');
+        }
+        //this.style.color = color;
+        audio.play();
     });
 }
