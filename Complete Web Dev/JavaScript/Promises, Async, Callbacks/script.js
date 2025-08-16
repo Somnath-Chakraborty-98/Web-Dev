7e0484d2860d270d656f7486850bbd093f565d69
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2025-08-16 22:50:06 +0530

    promises and promise chain

--- Complete Web Dev/JavaScript/Promises, Async, Callbacks/script.js
+++ Complete Web Dev/JavaScript/Promises, Async, Callbacks/script.js
@@ -1,5 +1,5 @@
 // set time out
-console.log("one");
+/*console.log("one");
 setTimeout( () =>{
     console.log("hello");
 }, 3000
@@ -39,3 +39,83 @@ getData(1, () =>{
 });
 
 //////////////////////////////////////////////////////////////////////
+// promise
+
+let promise = new Promise((resolve, reject) =>{
+    console.log("promise start");
+    resolve("success");
+    //reject("some error");
+});
+
+console.dir(promise);
+
+function getData1(nId, getNextData1) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data", nId);
+            resolve("Success");
+            console.dir(data);
+            if (getNextData1)
+                getNextData1();
+        }, 8000);
+    }
+    );
+}
+
+let data = getData1(123);
+
+const getPromise = () =>{
+    return new Promise((resolve, reject) =>{
+        console.log("PROMISE");
+        resolve("DONE");
+        //reject("NOT DONE");
+    });
+}
+
+let pro = getPromise();
+
+pro.then((res) => {
+    console.log("Completed", res);
+});
+
+pro.catch((err) => {
+    console.log("ERROR", err);
+});
+*/
+///////////////////////////////////////////////////////////////////////////
+// promise chain
+
+function asyncFun1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data 1");
+            resolve("Success");
+        }, 4000);
+    });
+}
+
+function asyncFun2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data 2");
+            resolve("Successfull");
+        }, 4000);
+    });
+}
+
+console.log("fetching data 1");
+let p1 = asyncFun1();
+p1.then((res) => {
+    console.log(res);
+    console.dir(p1);
+
+    console.log("fetching data 2");
+    let p2 = asyncFun2();
+    p2.then((res) => {
+        console.log(res);
+        console.dir(p1);
+    });
+});
+
+
+
