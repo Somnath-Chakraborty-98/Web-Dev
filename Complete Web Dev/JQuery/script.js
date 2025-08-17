7a4f91d5a90d0108300b8d4cedec39d0360badf7
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2025-08-17 17:48:34 +0530

    jQuery - completed

--- Complete Web Dev/JQuery/script.js
+++ Complete Web Dev/JQuery/script.js
@@ -1,2 +1,22 @@
 let h1 = $("h1");
-h1.css("color", "red");
+//h1.css("color", "red");
+console.log(h1.css("font-size"));
+
+h1.addClass("big-title");
+h1.text("Bye");
+
+let btns = $("button");
+console.dir(btns);
+//btns.css("background-color", "aquamarine");
+
+// btns.click(function (event) {
+//     alert("Button clicked: " + $(this).text()); // this = clicked button
+// });
+
+btns.on("mouseover", function (event) {
+    $(this).css("color", "blue");
+});
+
+btns.on("mouseout", function () {
+    $(this).css("color", "black");
+});
