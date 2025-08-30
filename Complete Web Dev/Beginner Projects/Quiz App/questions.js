d18f5afbb4e15f116440076a9b11bd5fec6e95e5
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2025-08-30 18:16:13 +0530

    Quiz completed

--- Complete Web Dev/Beginner Projects/Quiz App/questions.js
+++ Complete Web Dev/Beginner Projects/Quiz App/questions.js
@@ -3,26 +3,26 @@ let questions = [
   {
     question: "Which of the following is NOT a JavaScript data type?",
     answers: ["Number", "String", "Boolean", "Character"],
-    correct: "Character"
+    correct: 3
   },
   {
     question: "How do you write a single-line comment in JavaScript?",
     answers: ["<!-- comment -->", "// comment", "/* comment */", "# comment"],
-    correct: "// comment"
+    correct: 1
   },
   {
     question: "Which method is used to convert JSON data into a JavaScript object?",
     answers: ["JSON.parse()", "JSON.stringify()", "JSON.toObject()", "JSON.convert()"],
-    correct: "JSON.parse()"
+    correct: 0
   },
   {
     question: "What will `typeof null` return in JavaScript?",
     answers: ["'null'", "'object'", "'undefined'", "'boolean'"],
-    correct: "'object'"
+    correct: 1
   },
   {
     question: "Which keyword is used to declare a block-scoped variable in ES6?",
     answers: ["var", "let", "const", "both let and const"],
-    correct: "both let and const"
+    correct: 3
   }
 ];
--- Complete Web Dev/Beginner Projects/Quiz App/script.js
+++ Complete Web Dev/Beginner Projects/Quiz App/script.js
@@ -16,22 +16,67 @@ startQuiz.addEventListener('click', () => {
 
 let quesIndex = 0;
 let score = 0;
+let ansIndex = 0;
 
-function startWithTheQuiz(){
+function startWithTheQuiz() {
     quesIndex = 0;
     score = 0;
+    quizResDiv.style.display = "none";
 
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     let currentQuestion = questions[quesIndex];
     let answers = currentQuestion.answers;
     console.log(answers);
+
+    ansButtons[ansIndex].classList.remove("correct");
+    ansButtons[ansIndex].classList.remove("wrong");
+
     questionEle.innerText = (quesIndex + 1) + ". " + currentQuestion.question;
+    ansButtons.forEach(b => b.classList.remove("disabled"));
 
     ansButtons.forEach((btn, index) => {
         console.log(answers[index]);
         btn.innerText = answers[index];
+        btn.classList.remove("disabled");
     });
 }
+
+ansButtons.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        let currentQuestion = questions[quesIndex];
+        ansIndex = index;
+        console.log("User clicked:", currentQuestion.answers[index], "at index", index);
+        ansButtons.forEach(b => b.classList.add("disabled"));
+        if (currentQuestion.correct === index) {
+            console.log("Correct!");
+            btn.classList.add("correct");
+            ++score;
+        } else {
+            console.log("Wrong!");
+            btn.classList.add("wrong");
+        }
+
+        quesIndex++;
+        nextBtn.style.display = "block";
+        nextBtn.innerText = (4 === quesIndex) ? "Submit" : "Next";
+    });
+});
+
+
+nextBtn.addEventListener('click', () => {
+    if (questions.length === quesIndex) {
+        questionDiv.style.display = "none";
+        startQuiz.style.display = "block";
+        quizResDiv.style.display = "block";
+        quizRes.innerText = score + "/" + questions.length;
+        if (questions.length === score)
+            quizRes.style.color = "green";
+        else
+            quizRes.style.color = "red";
+    }
+    else
+        showQuestion();
+})
